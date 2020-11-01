e75d626087b1eeb47e888c0c66a9caae96f323c7
NarcizzeN <47037769+NarcizzeN@users.noreply.github.com>
2020-11-01 22:48:46 +0000

    after confirming that passwords match

--- process_registration.php
+++ process_registration.php
@@ -2,6 +2,7 @@
 $user = array();
 $user["EmailAddress"] = $_POST["email"];
 $user["Password"] = $_POST["Password"];
+$user["passwordConfirmation"] = $_POST["passwordConfirmation"];
 
 // TODO: Extract $_POST variables, check they're OK, and attempt to create
 // an account. Notify user of success/failure and redirect/give navigation 
@@ -19,6 +20,23 @@ or die('Error connecting to the server.' . sqlsrv_errors()['message']);
 
 if(!filter_var($user["EmailAddress"], FILTER_VALIDATE_EMAIL)) {
     exit('Invalid email address');
+}
+//$sel2= "SELECT FROM Users (EmailAddress)."
+//    "WHERE VALUES ('${user['EmailAddress']}"
+//query2 = "SELECT EmailAddress FROM databaseucl.dbo.Users WHERE EmailAddress = $user["EmailAddress"]"
+
+$params = array($user["EmailAddress"]);
+$tsql = "SELECT EmailAddress FROM databaseucl.dbo.Users
+        WHERE EmailAddress = ?";
+$cursorType = array("Scrollable" => SQLSRV_CURSOR_KEYSET);
+$select = sqlsrv_query($conn, $tsql, $params, $cursorType);
+
+if (sqlsrv_has_rows($select)) {
+    exit("This email is already being used");
+}
+
+if ($user["Password"] != $user["passwordConfirmation"]) {
+    exit("Password and Password confirmation did not match");
 }
 
 $query = "INSERT INTO Users (EmailAddress, Password)".
