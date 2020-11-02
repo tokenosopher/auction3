642a7eee7025c7ecdf1bfb66f0aca4f467900e10
Aleksei Sukhov <aleksei.sukhov.20@ucl.ac.uk>
2020-11-02 22:44:13 +0000

    Created a folder with a file to connect to Azure DB

--- db_con/db_li.php
+++ db_con/db_li.php
@@ -9,3 +9,21 @@ $connectionOptions = array(
 $conn = sqlsrv_connect($serverName, $connectionOptions)
     or die("Error connecting to the Azure DB" . sqlsrv_errors());
 echo("Connection to Azure DB successful!");
+/*  How to use this:
+    On top of the page paste this:
+
+    <?php include_once 'db_con/db_li.php'?>
+
+    What it does is essentially import the file and allows you
+    to use the $conn variable to connect to the database.
+
+    When you want to get data from the DB on webpage of your choice,
+    you can run:
+
+    sqlsrv_query($conn, $query);
+
+    Where $query is the query statement you had written in SQL.
+*/
+
+
+
