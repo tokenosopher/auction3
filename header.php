4c863ecd524822444c2ea206f17c0918784fb2a9
NarcizzeN <47037769+NarcizzeN@users.noreply.github.com>
2020-11-05 00:49:40 +0000

    Registration now done on Users2 When registration is performed, user is added to either buyers or sellers playing with creating sessions Removed dummy session login from header.php

--- header.php
+++ header.php
@@ -3,9 +3,9 @@
   // But eventually, with a database, these should be set automatically
   // ONLY after the user's login credentials have been verified via a 
   // database query.
-  session_start();
-  $_SESSION['logged_in'] = false;
-  $_SESSION['account_type'] = 'seller';
+//  session_start();
+//  $_SESSION['logged_in'] = false;
+//  $_SESSION['account_type'] = 'seller';
 ?>
 
 
@@ -22,7 +22,7 @@
   <!-- Custom CSS file -->
   <link rel="stylesheet" href="css/custom.css">
 
-  <title>[My Auction Site] <!--CHANGEME!--></title>
+  <title>Nicage Auction - Sell All Here</title>
 </head>
 
 
@@ -30,7 +30,7 @@
 
 <!-- Navbars -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light mx-2">
-  <a class="navbar-brand" href="#">Site Name <!--CHANGEME!--></a>
+  <a class="navbar-brand" href="#">Nicage Auction</a>
   <ul class="navbar-nav ml-auto">
     <li class="nav-item">
     
