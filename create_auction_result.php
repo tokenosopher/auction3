ef043f4d1d78647028f7ef307fdd7fe45cd077b6
imranhoss <73539265+imranhoss@users.noreply.github.com>
2020-11-07 21:48:41 +0000

    Imran_auction_fully_operational

--- create_auction_result.php
+++ create_auction_result.php
@@ -22,9 +22,6 @@
         //echo (($auction["ItemEndDate"])) . "<br>";
         //echo ($auction["Category"]) . "<br>";
         //echo (gettype($auction["Category"])) . "<br>";
-        echo ($current_date);
-        $current_date<($auction["ItemEndDate"])
-           or die("Your auction end date is in the past!");
 
 
         session_start();
