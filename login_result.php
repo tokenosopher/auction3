a793758238b7f747b733f2342e4c847557eeca41
NarcizzeN <47037769+NarcizzeN@users.noreply.github.com>
2020-11-08 15:30:41 +0000

    added the buyer id and seller id session variables (see checking_sessions.php for the variable names)

--- login_result.php
+++ login_result.php
@@ -21,26 +21,38 @@ $select = sqlsrv_query($conn, $tsql, $params, $cursorType);
 if (sqlsrv_num_rows($select) == 1){
         $row = sqlsrv_fetch_array($select);
         if (password_verify($user["password"], $row['Passwd'])) {
-            echo "You've logged in! You will be redirected in a moment";
+            echo '<section class="section"> ;
+            <div class="centered">
+            <h1><span>You are now logged in!</span></h1>
+            <p><span>You will be redirected in a second</span></p>
+            </div>';
             $_SESSION['user_id'] = $row['UserID'];
             $_SESSION['email'] = $row['EmailAddress'];
             $_SESSION['logged_in'] = true ;
             //querying to see if user is buyer or seller:
             $params_account_type = array($_SESSION['user_id']);
-            $query_account_type_buyer = "SELECT userID FROM Buyers
-                                          WHERE userID = ?";
+            $query_account_type_buyer = "SELECT userId, buyerId FROM Buyers
+                                          WHERE userId = ?";
             $querying_account_type_buyer = sqlsrv_query($conn, $query_account_type_buyer, $params_account_type, $cursorType);
             if (sqlsrv_has_rows($querying_account_type_buyer))
             {
                 $_SESSION['account_type'] = 'buyer';
+                $row_buyer = sqlsrv_fetch_array($querying_account_type_buyer);
+                $_SESSION['buyer_id'] = $row_buyer['buyerId'];
             } else {
                 $_SESSION['account_type'] = 'seller';
+                $query_account_type_seller = "SELECT userId, sellerId FROM Sellers
+                                          WHERE userId = ?";
+                $querying_account_type_seller = sqlsrv_query($conn, $query_account_type_seller, $params_account_type, $cursorType);
+                $row_seller = sqlsrv_fetch_array($querying_account_type_seller);
+                $_SESSION['seller_id'] = $row_seller['sellerId'];
+                echo $_SESSION['seller_id'];
             }
 
 //
 //            $_SESSION['account_type'] = "seller";
             // Redirect to index after 5 seconds
-            header("refresh:5;url=index.php");
+//            header("refresh:5;url=index.php");
 
         } else {
             echo "Invalid Password! Try again";
