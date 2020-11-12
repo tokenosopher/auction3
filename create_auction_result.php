b753310cfbfb4512eb929ef2a23ecb6f7b52b7e2
imranhoss <73539265+imranhoss@users.noreply.github.com>
2020-11-12 15:40:10 +0000

    Cleaned up files

--- create_auction_result.php
+++ create_auction_result.php
@@ -15,13 +15,7 @@
         $myvar2 = $_POST["auctionEndDate"];
         $auction["ItemEndDate"]=str_replace("T"," ",$myvar2); // converting html datetime into compatible form with the database
 
-        //echo ($auction["ItemTitle"]) ."<br>";
-        //echo ($auction["ItemDescription"]) . "<br>";
-        //echo (gettype($auction["ItemStartingPrice"])) . "<br>";
-        //echo (gettype($auction["ItemReservePrice"])) . "<br>";
-        //echo (($auction["ItemEndDate"])) . "<br>";
-        //echo ($auction["Category"]) . "<br>";
-        //echo (gettype($auction["Category"])) . "<br>";
+
         $current_date = date('Y-m-d H:i:s');
         $current_date<($auction["ItemEndDate"])
            or die("Your auction end date is in the past!");
@@ -29,41 +23,9 @@
         $auction['ItemStartingPrice']<($auction["ItemReservePrice"])
         or die("Your starting price is higher than your reserve price!");
 
-        // session_start();
 
-        //echo ( gettype($_SESSION['user_id']))."<br>";
-        //echo ($_SESSION['email'])."<br>";
-       // echo (gettype($_SESSION['logged_in']))."<br>";
-        //echo ($_SESSION['account_type'])."<br>";
-
-        //$query1 = "SELECT sellerId FROM databaseucl.dbo.Sellers
-        //WHERE (userId = '{$_SESSION['user_id']}')" ;
-
-        //$select1 = sqlsrv_query($conn, $query1,);
-        //$row1 = sqlsrv_fetch_array($select1);
-        //echo ($row1[0]);
-        //echo ($_SESSION['seller_id']);
         $myseller_id=$_SESSION['seller_id'];
-        //echo $myseller_id;
 
-
-
-
-
-
-        // DoneTODO: Extract $_POST variables, check they're OK, and attempt to create
-        // an account. Notify user of success/failure and redirect/give navigation
-        // options.
-
-        /*$serverName = "databaseucl.database.windows.net";
-        $connectionOptions = array(
-            "Database" => "databaseucl",
-            "Uid" => "narcis",
-            "PWD" => "P4ssword"
-        );
-
-        $conn = sqlsrv_connect($serverName, $connectionOptions)
-        or die('Error connecting to the server.' . sqlsrv_errors()['message']); */
 
         $query = "INSERT INTO databaseucl.dbo.AuctionItems(sellerId, ItemTitle, ItemDescription,ItemStartingPrice, ItemReservePrice,ItemEndDate,categoryId)
                     VALUES ( '${myseller_id}','${auction["ItemTitle"]}', '${auction["ItemDescription"]}', '${auction["ItemStartingPrice"]}','${auction["ItemReservePrice"]}', '${auction["ItemEndDate"]}', '${auction["Category"]}' )";
@@ -77,24 +39,6 @@
         // This function takes the form data and adds the new auction to the database.
 
 
-        /* TODO #1: Connect to MySQL database (perhaps by requiring a file that
-                    already does this). */
-
-
-        /* TODO #2: Extract form data into variables. Because the form was a 'post'
-                    form, its data can be accessed via $POST['auctionTitle'],
-                    $POST['auctionDetails'], etc. Perform checking on the data to
-                    make sure it can be inserted into the database. If there is an
-                    issue, give some semi-helpful feedback to user. */
-
-
-        /* TODO #3: If everything looks good, make the appropriate call to insert
-                    data into the database. */
-
-
-
-
-        // If all is successful, let user know.
         ?>
 
     </div>
