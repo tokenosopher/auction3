805ebcacc80f3511314dbc16eed9b16e55f7fc9c
imranhoss <73539265+imranhoss@users.noreply.github.com>
2020-11-12 22:06:47 +0000

    Added PHPMailer and script to add mails

---
+++ testing_mail.php
@@ -0,0 +1,25 @@
+<?php
+require_once('PHPMailer/PHPMailerAutoload.php');
+
+$mail = new PHPMailer();
+$mail -> isSMTP();
+$mail -> SMTPAuth= true;
+$mail -> SMTPSecure = 'ssl';
+$mail -> Host = 'smtp.gmail.com';
+$mail -> Port = '465';
+$mail -> isHTML();
+$mail -> Username = 'db.groupproject9@gmail.com';
+$mail -> Password = 'DreamTeam1';
+$mail -> SetFrom( 'db.groupproject9@gmail.com');
+$mail -> Subject = " Checking that this email works ";
+$mail -> Body = ' Hi, this is test email ';
+$mail -> AddAddress ('ihossain.falcone@gmail.com');
+
+$mail -> Send();
+
+echo 'Email sent successfully';
+
+
+
+
+?>
