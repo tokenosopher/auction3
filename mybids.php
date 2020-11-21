e19288fbd56d762ca60bdacdbc30cac3dd247e9e
chenu <ucabcnd@ucl.ac.uk>
2020-11-21 19:13:34 +0000

    Commented code.
    Cleaned up some code on auction functions and listing . php

--- mybids.php
+++ mybids.php
@@ -10,23 +10,21 @@
   // This page is for showing a user the auctions they've bid on.
   // It will be pretty similar to browse.php, except there is no search bar.
   // This can be started after browse.php is working with a database.
-  // Feel free to extract out useful functions from browse.php and put them in
-  // the shared "utilities.php" where they can be shared by multiple files.
   
   
-  // TODO: Check user's credentials (cookie/session).
+  // Checks user's credentials (cookie/session).
 if (isset($_SESSION['logged_in']) and $_SESSION['account_type'] == 'buyer') {
     $user_id = $_SESSION['user_id'];
     $buyer_id = $_SESSION['buyer_id'];
-    // TODO: Perform a query to pull up the auctions they've bidded on.
 
+    // Performs a query to pull up the auctions user has bidded on.
     $getResults = getmybids();
-
+    // Default message if user hasn't bidded on anything
     if(!sqlsrv_fetch_array($getResults)['itemId']){
         echo "You haven't made any bids! Start bidding now!";
     }
 
-    // TODO: Loop through results and print them out as list items.
+    // Loops through results and prints them out as list items.
     WHILE ($row = sqlsrv_fetch_array($getResults)) {
 
         $item_id = $row['itemId'];
